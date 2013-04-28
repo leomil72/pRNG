e67debc6cacb8435556e0f59494b1e705fe9697c
leomil72 <leonardo@leonardomiliani.com>
2013-04-28 11:43:14 +0200

    New version 1.1 - now it can retun an unsigned long too

--- pRNG.h
+++ pRNG.h
@@ -1,23 +1,23 @@
 /*
 	pRNG.h - <p>retty <R>andom <N>umber <G>enerator
-	
+
     pRNG is a simple pseudo-random number generator. It uses
     a mechanism based on an interrupt raised by the WatchDog
     Timer of the microcontroller to collect entropy and a
     Galois 32 bit LFSR (Linear Feedback Shift Register) to
     distribuite it into a 10-bytes pool.
-    
+
     ** IMPORTANT: pRNG is NOT intended for use in critical applications!! **
-    
+
     For more infos, please read the README.txt file.
 
 	Written by Leonardo Miliani <www DOT leonardomiliani DOT com>
-    
+
     The latest version of this library can be found at:
     http://www.leonardomiliani.com/
     or at:
     https://github.com/leomil72
-    	
+
   	This library is free software; you can redistribute it and/or
 	modify it under the terms of the GNU General Public
 	License as published by the Free Software Foundation; either
@@ -25,10 +25,10 @@
 
     You should have received a copy of the GNU General Public License
     along with this library.  If not, see <http://www.gnu.org/licenses/>
-    
+
 	This library is distributed in the hope that it will be useful,
 	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 
 */
 
@@ -38,7 +38,7 @@
 
 
 //library version
-#define pRNG_VERSION 100
+#define pRNG_VERSION 110
 
 
 //this library is compatible both with Arduino <=0023 and Arduino >=100
@@ -49,7 +49,7 @@
 #endif
 
 
-//check MCU compatibility - pRNG won't work on Atmega8 
+//check MCU compatibility - pRNG won't work on Atmega8
 #if defined (__AVR_ATmega8__) || defined (__AVR_ATmega8A__)
 #error Sorry, this MCU is not supported (lack of interrupt vector for WDT)!
 #endif
@@ -57,12 +57,13 @@
 
 //pRNG class
 class pRNG {
-	public: 
+	public:
 		//public methods
 		pRNG();
         void begin();
 		uint8_t getRndByte(void);
         unsigned int getRndInt(void);
+        unsigned long getRndLong(void);
 	private:
         //private methods
         void _setWDT();
